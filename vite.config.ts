f42bf5ab2547a8ce6681f94971ad5a0c2eecac38
dev patel <patel.07.dsp@gmail.com>
2025-08-31 00:48:07 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,8 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig(({ plugins: [react()],
+  base: "/hydromap/",  // 👈 important for GitHub Pages }) => ({
   server: {
     host: "::",
     port: 8080,
